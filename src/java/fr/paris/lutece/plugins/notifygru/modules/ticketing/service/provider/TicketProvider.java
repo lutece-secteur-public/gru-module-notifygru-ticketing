558852dfe77455c373312026ade0114f77122719
Cyril Moire <cyril.moire@atos.net>
2017-02-24 14:46:49 +0100

    MPTCKETING-34 : add Javadoc

--- src/java/fr/paris/lutece/plugins/notifygru/modules/ticketing/service/provider/TicketProvider.java
+++ src/java/fr/paris/lutece/plugins/notifygru/modules/ticketing/service/provider/TicketProvider.java
@@ -1,3 +1,36 @@
+/*
+ * Copyright (c) 2002-2017, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
 package fr.paris.lutece.plugins.notifygru.modules.ticketing.service.provider;
 
 import java.util.ArrayList;
@@ -14,6 +47,10 @@ import fr.paris.lutece.plugins.workflow.modules.notifygru.service.provider.Notif
 import fr.paris.lutece.plugins.workflowcore.business.resource.ResourceHistory;
 import fr.paris.lutece.portal.service.i18n.I18nService;
 
+/**
+ * This class represents a provider for a {@link Ticket} object
+ *
+ */
 public class TicketProvider implements IProvider
 {
     private static final String MESSAGE_MARKER_TICKET_REFERENCE = "ticketing.manage_tickets.columnReference";
@@ -36,53 +73,83 @@ public class TicketProvider implements IProvider
 
     private Ticket _ticket;
 
+    /**
+     * Constructor
+     * 
+     * @param resourceHistory
+     *            the resource history. Corresponds to the {@code Ticket} object containing the data to provide
+     */
     public TicketProvider( ResourceHistory resourceHistory )
     {
         _ticket = TicketHome.findByPrimaryKey( resourceHistory.getIdResource( ) );
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String provideDemandId( )
     {
         return String.valueOf( _ticket.getId( ) );
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String provideDemandTypeId( )
     {
         return String.valueOf( TicketTypeHome.findByPrimaryKey( _ticket.getIdTicketType( ) ).getDemandTypeId( ) );
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String provideDemandReference( )
     {
         return _ticket.getReference( );
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String provideCustomerConnectionId( )
     {
         return _ticket.getGuid( );
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String provideCustomerId( )
     {
         return _ticket.getCustomerId( );
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String provideCustomerEmail( )
     {
         return _ticket.getEmail( );
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String provideCustomerMobilePhone( )
     {
         return _ticket.getMobilePhoneNumber( );
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Collection<NotifyGruMarker> provideMarkerValues( )
     {
@@ -127,6 +194,11 @@ public class TicketProvider implements IProvider
         return collectionNotifyGruMarkers;
     }
 
+    /**
+     * Gives the marker descriptions
+     * 
+     * @return the marker descritions
+     */
     public static Collection<NotifyGruMarker> getProviderMarkerDescriptions( )
     {
         Collection<NotifyGruMarker> collectionNotifyGruMarkers = new ArrayList<>( );
@@ -152,6 +224,15 @@ public class TicketProvider implements IProvider
         return collectionNotifyGruMarkers;
     }
 
+    /**
+     * Creates a {@code NotifyGruMarker} object with the specified marker and value.
+     * 
+     * @param strMarker
+     *            the marker
+     * @param strValue
+     *            the value to inject into the {@code NotifyGruMarker} object
+     * @return the {@code NotifyGruMarker} object
+     */
     private static NotifyGruMarker createMarkerValues( String strMarker, String strValue )
     {
         NotifyGruMarker notifyGruMarker = new NotifyGruMarker( strMarker );
@@ -160,6 +241,15 @@ public class TicketProvider implements IProvider
         return notifyGruMarker;
     }
 
+    /**
+     * Creates a {@code NotifyGruMarker} object with the specified marker and description.
+     * 
+     * @param strMarker
+     *            the marker
+     * @param strDescription
+     *            the description to inject into the {@code NotifyGruMarker} object
+     * @return the {@code NotifyGruMarker} object
+     */
     private static NotifyGruMarker createMarkerDescriptions( String strMarker, String strDescription )
     {
         NotifyGruMarker notifyGruMarker = new NotifyGruMarker( strMarker );
--- src/java/fr/paris/lutece/plugins/notifygru/modules/ticketing/service/provider/TicketProviderManager.java
+++ src/java/fr/paris/lutece/plugins/notifygru/modules/ticketing/service/provider/TicketProviderManager.java
@@ -1,3 +1,36 @@
+/*
+ * Copyright (c) 2002-2017, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
 package fr.paris.lutece.plugins.notifygru.modules.ticketing.service.provider;
 
 import java.util.ArrayList;
@@ -10,17 +43,35 @@ import fr.paris.lutece.plugins.workflowcore.business.resource.ResourceHistory;
 import fr.paris.lutece.plugins.workflowcore.service.task.ITask;
 import fr.paris.lutece.portal.service.i18n.I18nService;
 
+/**
+ * <p>
+ * This class represents a provider manager for {@link fr.paris.lutece.plugins.ticketing.business.ticket.Ticket Ticket} objects
+ * </p>
+ * <p>
+ * Same provider for all {@code Ticket} objects
+ * </p>
+ *
+ */
 public class TicketProviderManager extends AbstractProviderManager
 {
     private static final String PROVIDER_ID = "ticket";
 
     private static final String MESSAGE_PROVIDER_LABEL = "module.notifygru.ticketing.module.provider.ticketing";
 
+    /**
+     * Constructor
+     * 
+     * @param strId
+     *            the id of this provider manager
+     */
     public TicketProviderManager( String strId )
     {
         super( strId );
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Collection<ProviderDescription> getAllProviderDescriptions( ITask task )
     {
@@ -31,6 +82,9 @@ public class TicketProviderManager extends AbstractProviderManager
         return collectionProviderDescriptions;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public ProviderDescription getProviderDescription( String strProviderId )
     {
@@ -42,6 +96,9 @@ public class TicketProviderManager extends AbstractProviderManager
         return providerDescription;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public IProvider createProvider( String strProviderId, ResourceHistory resourceHistory )
     {
