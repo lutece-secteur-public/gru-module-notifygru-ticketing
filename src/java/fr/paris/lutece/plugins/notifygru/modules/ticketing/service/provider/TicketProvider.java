01ff730839811845f7f420cf32b1d9c17bb9e804
killianMorvan <killian.morvan@accenture.com>
2018-02-26 16:38:13 +0100

    TICKETING-376 - Add signets priority and criticality

--- src/java/fr/paris/lutece/plugins/notifygru/modules/ticketing/service/provider/TicketProvider.java
+++ src/java/fr/paris/lutece/plugins/notifygru/modules/ticketing/service/provider/TicketProvider.java
@@ -35,15 +35,18 @@ package fr.paris.lutece.plugins.notifygru.modules.ticketing.service.provider;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang.StringUtils;
 
 import fr.paris.lutece.plugins.notifygru.modules.ticketing.Constants;
 import fr.paris.lutece.plugins.ticketing.business.category.TicketCategoryType;
 import fr.paris.lutece.plugins.ticketing.business.category.TicketCategoryTypeHome;
 import fr.paris.lutece.plugins.ticketing.business.ticket.Ticket;
+import fr.paris.lutece.plugins.ticketing.business.ticket.TicketCriticality;
 import fr.paris.lutece.plugins.ticketing.business.ticket.TicketHome;
 import fr.paris.lutece.plugins.ticketing.service.category.TicketCategoryService;
 import fr.paris.lutece.plugins.ticketing.web.TicketingConstants;
@@ -86,6 +89,9 @@ public class TicketProvider implements IProvider
     private static final String MESSAGE_MARKER_USER_CITY                = "ticketing.create_ticket.labelCity";
     private static final String MESSAGE_MARKER_USER_PRIORITY         	= "ticketing.view_ticket_details.labelTicketPriority";
     private static final String MESSAGE_MARKER_USER_CRITICALITY         = "ticketing.view_ticket_details.labelTicketCriticality";
+    
+    private static final String MESSAGE_MARKER_PRIORITY_LABEL           = "module.workflow.ticketing.task_ticket_email_external_user_config.label_entry.priority_label";
+    private static final String MESSAGE_MARKER_CRITICALITY_LABEL        = "module.workflow.ticketing.task_ticket_email_external_user_config.label_entry.criticality_label";
 
     private static final String SEMICOLON                               = ";";
 
@@ -258,6 +264,22 @@ public class TicketProvider implements IProvider
             collectionNotifyGruMarkers.add( createMarkerValues( Constants.MARK_USER_POSTAL_CODE, StringEscapeUtils.escapeHtml( _ticket.getTicketAddress( ).getPostalCode( ) ) ) );
             collectionNotifyGruMarkers.add( createMarkerValues( Constants.MARK_USER_CITY, StringEscapeUtils.escapeHtml( _ticket.getTicketAddress( ).getCity( ) ) ) );
 
+        }
+        
+        if( _ticket.getPriority( ) != 0 ) {
+            TicketCriticality priority = TicketCriticality.valueOf( _ticket.getPriority( ) );
+            String priorityLabel = StringUtils.EMPTY;
+            priorityLabel = I18nService.getLocalizedString(MESSAGE_MARKER_PRIORITY_LABEL, I18nService.getDefaultLocale( ) ) + priority.getLocalizedMessage( Locale.FRENCH );
+            
+            collectionNotifyGruMarkers.add( createMarkerValues( Constants.MARK_USER_PRIORITY, priorityLabel ) );
+        }
+        
+        if( _ticket.getCriticality( ) != 0 ) {
+            TicketCriticality criticality = TicketCriticality.valueOf( _ticket.getCriticality( ) );
+            String criticalityLabel = StringUtils.EMPTY;
+            criticalityLabel = I18nService.getLocalizedString(MESSAGE_MARKER_CRITICALITY_LABEL, I18nService.getDefaultLocale( ) ) + criticality.getLocalizedMessage( Locale.FRENCH );
+                
+            collectionNotifyGruMarkers.add( createMarkerValues( Constants.MARK_USER_CRITICALITY, criticalityLabel));
         }
 
         return collectionNotifyGruMarkers;
